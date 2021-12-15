9124cdb955f047aee3ae265fdac0fa545ffc9704
Your Name <you@example.com>
2021-12-16 00:37:49 +0200

    add some files

--- server.js
+++ server.js
@@ -24,9 +24,7 @@ app.use(express.json());
 app.use(cors());
 app.use(corsMiddleware);
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 app.use(authRoute);
 app.use(userRoute);
